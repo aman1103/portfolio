453d153d9ea71b3f16c39fd747e6225d124a2b83
Aman Gaharwar <percival@pop-os.localdomain>
2022-03-29 15:37:18 +0530

    Added color change on scroll

---
+++ src/component/app.js
@@ -0,0 +1,21 @@
+import ResponsiveAppBar from './responsiveAppBar';
+function App() {
+  return (
+    <>
+    <ResponsiveAppBar/>
+    <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam maximus metus eget sagittis pretium. Sed iaculis libero eu scelerisque vulputate. Sed lacinia sapien nulla, eu pulvinar erat dapibus ac. Quisque at blandit ligula, quis vehicula justo. Etiam ultrices quis nisl vel sodales. Vivamus a consequat justo. Ut sagittis finibus lacinia. Sed dignissim vulputate dapibus. Quisque volutpat magna eu libero facilisis, id euismod erat sagittis. Cras ut bibendum quam. Ut egestas sagittis mauris, non volutpat tellus viverra dapibus. Suspendisse a nisl eu velit condimentum tincidunt.
+
+Nam dolor dolor, porta eget diam non, viverra congue nisl. Curabitur dignissim, mi nec ultricies ullamcorper, tortor lorem congue ipsum, vitae semper sem tortor eu lorem. Nunc sed accumsan elit. Sed pharetra et risus eget aliquam. Praesent commodo augue vel sodales dignissim. In laoreet nibh non ex dignissim semper. Nam lacinia ornare lobortis. Etiam lobortis nec orci at volutpat. Cras convallis erat velit, eu facilisis elit facilisis vitae. Vivamus vitae magna non sem vehicula ornare quis eu mi. Integer id risus dictum, laoreet quam eget, molestie urna. Curabitur malesuada dui augue, vel rhoncus ipsum interdum sit amet. Quisque tristique dapibus nunc vel blandit. Vivamus a neque consectetur, tincidunt urna non, tincidunt dui. Ut ultrices ex id eros consequat posuere. Pellentesque blandit sem nec suscipit consectetur.
+
+Curabitur fermentum leo sit amet elementum ultricies. Fusce molestie vehicula arcu, in aliquet eros congue sit amet. Suspendisse potenti. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras maximus ultricies scelerisque. Fusce rhoncus tellus diam, sed mattis purus malesuada quis. Nunc sapien tellus, egestas et leo non, vulputate rhoncus tortor.
+
+Mauris in leo a lacus varius rhoncus. Vivamus dignissim ante et mauris hendrerit, id varius nibh facilisis. Pellentesque ac consequat nibh. Cras rhoncus nulla ac semper tempus. Duis ut ex ullamcorper, commodo risus sed, sodales augue. Praesent interdum sem urna, nec tempor dui lobortis facilisis. Vestibulum in ex id turpis aliquet vehicula. Duis in sem efficitur, convallis ipsum vel, interdum turpis. Aliquam pharetra nulla eu nisl venenatis, sed pretium ante laoreet. In hendrerit vel nulla id convallis. Sed vel est magna. In sed dui elit. Donec non diam in turpis mollis mollis.
+
+Donec quis mollis nunc. Nunc id consequat tellus, nec pharetra metus. Nullam eget urna vitae nunc accumsan laoreet pretium pretium ex. Fusce posuere, mi et pellentesque pulvinar, metus ex pharetra ipsum, a ullamcorper massa ante quis velit. Aliquam sed lacus mi. Proin imperdiet lectus id blandit convallis. Nulla mollis rutrum maximus. Aenean varius risus quis ex maximus rutrum. Nunc dui mauris, viverra at lacinia at, consectetur ut nulla. Ut pellentesque ex vel ipsum semper congue. In tincidunt consequat tellus, non eleifend nisi consectetur non. Suspendisse id nibh justo. Sed imperdiet ligula eget velit congue egestas. Duis tortor neque, condimentum ut ante vel, laoreet tristique tellus.
+
+</h1>
+    </>
+  );
+}
+
+export default App;
